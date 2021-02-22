3404a94eae42e97919b65ac8b08ed8e32c78b512
mvalentine65 <mvalentine8@hawkmail.hccfl.edu>
2021-02-21 20:58:16 -0500

    Moves GUI.java to bitsplease.Project; Adds comments to adminGUI.java

--- bitsplease.Project3/src/test/java/adminGUI.java
+++ bitsplease.Project3/src/test/java/adminGUI.java
@@ -1,27 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
 
-
 public class adminGUI {
     public static void main(String[] args) {
+        // create window
         Container con = new Container();
         JFrame adminWindow = new JFrame();
+        //set fonts
         Font titleFont = new Font("Times New Roman", Font.BOLD, 40);
         Font anchorFont = new Font("Times New Roman", Font.PLAIN, 16);
-        adminWindow.setSize(800,600 );
+        // set layout and resolutoin
+        adminWindow.setSize(800, 600);
         adminWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         adminWindow.setLayout(null);
         adminWindow.setVisible(true);
+        // make content pane
         con = adminWindow.getContentPane();
+        // add title
         JLabel titleLabel = new JLabel("BitsPlease Search Engine");
         titleLabel.setFont(titleFont);
         JPanel titlePanel = new JPanel();
         con.add(titlePanel);
         titlePanel.setBounds(40, 10, 700, 100);
         titlePanel.add(titleLabel);
+        // add buttons
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.black);
-        buttonPanel.setBounds(200,400, 400,50);
+        buttonPanel.setBounds(200, 400, 400, 50);
         con.add(buttonPanel);
         JButton addFile = new JButton("Add File");
         JButton updateFile = new JButton("Update");
@@ -29,8 +34,9 @@ public class adminGUI {
         buttonPanel.add(addFile);
         buttonPanel.add(updateFile);
         buttonPanel.add(removeFile);
+        // adds flavor text
         JPanel anchorPanel = new JPanel();
-        anchorPanel.setBounds(200,500,400, 50);
+        anchorPanel.setBounds(200, 500, 400, 50);
         JLabel anchorText = new JLabel("Searching since 2021");
         anchorText.setFont(anchorFont);
         anchorPanel.add(anchorText);
