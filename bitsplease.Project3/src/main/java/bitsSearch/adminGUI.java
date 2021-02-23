65103b8403fbbfc226e040e49b12e0b42dfb9e38
Jennifer Dominguez <jendominguez89@yahoo.com>
2021-02-23 16:49:20 -0500

    Removed black table behind buttons.

--- bitsplease.Project3/src/main/java/bitsSearch/adminGUI.java
+++ bitsplease.Project3/src/main/java/bitsSearch/adminGUI.java
@@ -5,13 +5,17 @@ import java.awt.*;
 
 public class adminGUI {
     public adminGUI() {
+
         // create window
         Container con = new Container();
         JFrame adminWindow = new JFrame();
+
         //set fonts
         Font titleFont = new Font("Times New Roman", Font.BOLD, 40);
         Font anchorFont = new Font("Times New Roman", Font.PLAIN, 16);
+
         // set layout and resolution
+
         adminWindow.setSize(800, 600);
         /*
          *  Turns out when you close the admin window, the search window
@@ -21,8 +25,10 @@ public class adminGUI {
         adminWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         adminWindow.setLayout(null);
         adminWindow.setVisible(true);
+
         // make content pane
         con = adminWindow.getContentPane();
+
         // add title
         JLabel titleLabel = new JLabel("BitsPlease Search Engine");
         titleLabel.setFont(titleFont);
@@ -30,9 +36,9 @@ public class adminGUI {
         con.add(titlePanel);
         titlePanel.setBounds(40, 10, 700, 100);
         titlePanel.add(titleLabel);
+
         // add buttons
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(Color.black);
         buttonPanel.setBounds(200, 400, 400, 50);
         con.add(buttonPanel);
         JButton addFile = new JButton("Add File");
@@ -41,6 +47,7 @@ public class adminGUI {
         buttonPanel.add(addFile);
         buttonPanel.add(updateFile);
         buttonPanel.add(removeFile);
+
         // adds flavor text
         JPanel anchorPanel = new JPanel();
         anchorPanel.setBounds(200, 500, 400, 50);
