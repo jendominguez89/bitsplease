bea45de48cbeca2430444e3ac45ae66f6410638d
mvalentine65 <mvalentine8@hawkmail.hccfl.edu>
2021-02-23 00:30:07 -0500

    Changes folder structure; Requires pom.xml to turn tests

---
+++ bitsplease.Project3/src/test/java/bitsSearch/GUITest.java
@@ -0,0 +1,14 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/*  Much like a Hobbsian Leviathan, this test may appear to have power
+    where there is in fact none. I think a programmer should appreciate
+    examples of symbols and references.
+ */
+
+public class GUITest {
+    public void testRun() {
+        assertTrue( true );
+    }
+}
