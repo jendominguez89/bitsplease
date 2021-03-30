2a0dc6c1589e5e99512152fda24838862469d30a
mvalentine65 <mvalentine8@hawkmail.hccfl.edu>
2021-03-30 15:52:03 -0400

    Adds tests

--- bitsplease.Project3/src/test/java/bitsSearch/GUITest.java
+++ bitsplease.Project3/src/test/java/bitsSearch/GUITest.java
@@ -1,4 +1,4 @@
-
+package bitsSearch;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -8,7 +8,36 @@ import org.junit.Test;
  */
 
 public class GUITest {
+    @Test
     public void testRun() {
         assertTrue( true );
     }
+
+    /*
+     * These test functions (briefly) make an instance of each window.
+     * This is important becuase we have a lot of dependencies in the
+     * project now and I don't want to manually add everything to the
+     * classpath and manually compile everything for every change. In
+     * particular, the Gson jar will be a pain.
+     *
+     * Instead, we use test methods and let Maven automate all that.
+     *
+     * When we add new features later on, we can (and should) write more
+     * tests to make sure everything works right. I may look into Jenkins
+     * or something.
+     */
+    @Test
+    public void testSearchWindow() {
+        GUI testGui = new GUI();
+        assertTrue(testGui != null);
+        }
+
+    @Test
+    public void testAdminWindow() {
+        AdminGUI adminTest = new AdminGUI();
+        assertTrue(adminTest != null);
+    }
+
 }
+
+
